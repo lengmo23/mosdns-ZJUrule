b90c67442db17c1b37641b50425615c558ad63c3
lengmo23 <77873203+lengmo23@users.noreply.github.com>
2024-01-24 20:32:46 +0800

    Update MOSIP.sh

--- MOSIP.sh
+++ MOSIP.sh
@@ -8,5 +8,7 @@ wget -O /etc/mosdns/rule/proxy-list.txt https://raw.githubusercontent.com/Loyals
 wget -O /etc/mosdns/rule/cn.txt https://raw.githubusercontent.com/Loyalsoldier/geoip/release/text/cn.txt
 wget -O /etc/mosdns/rule/direct-list.txt https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/direct-list.txt
 wget -O /etc/mosdns/rule/reject-list.txt https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/reject-list.txt
-wget -O /etc/mosdns/rule/apple-cn.txt https://github.com/Loyalsoldier/v2ray-rules-dat/blob/release/apple-cn.txt
+wget -O /etc/mosdns/rule/mosdns_adrules.txt https://raw.githubusercontent.com/Cats-Team/AdRules/main/mosdns_adrules.txt
+wget -O /etc/mosdns/rule/apple-cn.txt https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/apple-cn.txt
 wget -O /etc/mosdns/rule/ZJU-list.txt https://raw.githubusercontent.com/lengmo23/mosdns-ZJUrule/main/ZJU-list.txt
+wget -O /etc/mosdns/rule/google-cn.txt https://raw.githubusercontent.com/Loyalsoldier/v2ray-rules-dat/release/google-cn.txt
